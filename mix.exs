ece5b5ad8e577cb77bc3aa29b727a9bf540c3dc8
Enrique Fernández <enrique@bluelabs.eu>
2021-04-01 21:24:27 +0200

    Update ExDoc version

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Exenum.Mixfile do
     [
       # Test dependencies
       {:credo, "~> 0.4", only: [:dev, :test]},
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:excoveralls, "~> 0.5", only: :test}
     ]
   end
